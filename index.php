2e1b24078c6b03693aaaeafb811985fb0a68be33
SuperSle <sl@internethic.com>
2014-04-08 17:59:41 +0200

    Refonte de la home
    Modification de la Home : suppression des "Test" + Texte de présentation

--- index.php
+++ index.php
@@ -23,38 +23,55 @@
     ?>
 
     <div class="row">
-        <div class="large-6 medium-6 columns">
+        <div class="large-8 columns">
             <div class="callout panel radius">
-                <ul>
-                    <li>Item d'Exemple : <a href="./item.php?id=3870">Plastron Lunaire (id: 3870)</a></li>
-                    <li><a href="./allitem.php">Voir tous les objets</a></li>
-                    <li><a href="./allpano.php">Voir toutes les panoplies</a></li>
-                    <li><a href="./analyse.php">Voir les Tarifs HDV</a></li>
-                    <li><a href="./favoris.php">Voir les objets favoris</a></li>
-                    <li><a href="http://docs.google.com/spreadsheet/ccc?key=0Aj3D1TOIZvCAdFZBbjdKQlI1enNjVHB2VzZGTjMyS0E#gid=1">Suivi de Projet</a></li>
-                    <li><a href="http://docs.google.com/document/d/1sBwUq6eqS8hd1yzhnrKYGlHCK_ERIEW1D38d7e6CAIQ">Cahier des charges</a></li>
-                </ul>
-                <span style="font-size:60%">Dernier import des Items : <?php print BDD::get_last_item_import(); ?><br> 
-                Dernier import des Prix : <?php print BDD::get_last_price_import(); ?></span> 
+                <p>Waktisanat un Fan site pour vous assister dans l'<strong>Artisanat du jeu Wakfu</strong>.</p>
+                <p>Le site permet de suivre l'<strong>historique de l'hôtel de vente d'Aerafal</strong>.</p>
+                <p>Une <strong>encyclopédie présentant tous les objets de Wakfu et leur recette</strong> est également à votre disposition.</p>
+                <p>Un <strong>moteur de recherche avancé</strong> vous permettra de trouver rapidement les objets souhaités.</p>
+                <p>Vous pouvez ajouter les <strong>objets en Favori</strong> afin de pouvoir les retrouver plus efficacement par la suite.</p>
+            </div>
+            
+            <div class="callout panel radius">
+                <p>N'hésitez pas à nous faire savoir si vous rencontrez des difficultés ou des erreurs...</p>
+                <p>Nous sommes également ouvert à vos propositions en terme de design du site <em>(oui ce n'est pas notre fort)</em>, mais aussi sur des fonctionnalités ou des outils à mettre en place toujours dans le but de vous faciliter la vie.</p>
             </div>
 		</div>
         
-        <div class="large-6 medium-6 columns">
-            <ul class="button-group">
-                <li><a class="button" href="./allitem.php"><strong><?php echo $countItem; ?></strong><br /> Items</a></li>
-                <li><a class="button" href="./allpano.php"><strong><?php echo $countPano; ?></strong><br /> Panoplies</a></li>
-		        </ul>
-            <div class="row collapse">
-                <form action="search.php" method=GET>
-                    <div class="small-11 columns" style="padding:0;">
-                        <input type="text" id=item name=item placeholder="Chercher...">
-                    </div>
-                    <div class="small-1 columns" style="padding:0;">
-                        <span class="postfix"><img src="./images/search_icon.gif" style="cursor:pointer" onclick="submit();"></span>
-                    </div>
-                    
-                </form>
-            </div>
+        <div class="large-4 columns">
+            <fieldset>
+                <legend>Waktisanat, c'est :</legend>
+                <ul>
+                    <li><a href="./allitem.php"><strong><?php echo $countItem; ?></strong> Items</a></li>
+                    <li><a href="./allpano.php"><strong><?php echo $countPano; ?></strong> Panoplies</a></li>
+                    <li><a href="./favoris.php">Objets Favoris</a></li>
+                    <li><a href="./searchadv.php">Recherche avancée</a></li>
+                    <li><a href="./analyse.php">Suivi HDV d'Aerafal</a></li>
+                    <li>Derniers imports :
+                        <ul>
+                            <li>Items : <?php print BDD::get_last_item_import(); ?></li>
+                            <li>Prix : <?php print BDD::get_last_price_import(); ?></li>
+                        </ul>
+                    </li>
+                </ul>
+            </fieldset>
+
+            <fieldset>
+                <legend>Prochainement, ce sera :</legend>
+                <ul>
+                    <li>Export (GDoc/xls) d'une recette pour le suivi de ressources</li>
+                    <li>Comparaison de différents équipements et panoplies</li>
+                    <li>A suivre...</li>
+                </ul>
+            </fieldset>
+            
+            <fieldset>
+                <legend>Le coin des Devs</legend>
+                <ul>
+                    <li><a href="http://docs.google.com/spreadsheet/ccc?key=0Aj3D1TOIZvCAdFZBbjdKQlI1enNjVHB2VzZGTjMyS0E#gid=1">Suivi de Projet</a></li>
+                    <li><a href="http://docs.google.com/document/d/1sBwUq6eqS8hd1yzhnrKYGlHCK_ERIEW1D38d7e6CAIQ">Cahier des charges</a></li>
+                </ul>
+            </fieldset>
         </div>
     </div>
     
--- parts/menu_short.php
+++ parts/menu_short.php
@@ -1,13 +1,23 @@
 <div class="row">
-    <div class="large-12 columns">
+    <div class="large-9 columns">
         <ul class="inline-list menu_short">
-            <li><a href="./" class="button radius tiny diamond <?php if( $currentMenu == "home" ){ echo "current"; } ?>">Accueil</a></li>
+            <!--<li><a href="./" class="button radius tiny diamond <?php if( $currentMenu == "home" ){ echo "current"; } ?>">Accueil</a></li>-->
             <li><a href="./allitem.php" class="button radius tiny diamond <?php if( $currentMenu == "obj" ){ echo "current"; } ?>">Objets</a></li>
             <li><a href="./allpano.php" class="button radius tiny diamond <?php if( $currentMenu == "pano" ){ echo "current"; } ?>">Panoplies</a></li>
             <li><a href="./analyse.php" class="button radius tiny diamond <?php if( $currentMenu == "tarif" ){ echo "current"; } ?>">Tarifs HDV</a></li>
             <li><a href="./favoris.php" class="button radius tiny diamond <?php if( $currentMenu == "fav" ){ echo "current"; } ?>">Favoris</a></li>
             <li><a href="./searchadv.php" class="button radius tiny diamond <?php if( $currentMenu == "search" ){ echo "current"; } ?>">Recherche avancée</a></li>
         </ul>
+    </div>
+    <div class="large-3 columns">
+        <form class="search_short" action="search.php" method=GET>
+            <div class="small-11 columns" style="padding:0;">
+                <input type="text" id=item name=item placeholder="Chercher...">
+            </div>
+            <div class="small-1 columns" style="padding:0;">
+                <span class="postfix"><img src="./images/search_icon.gif" style="cursor:pointer" onclick="submit();"></span>
+            </div>
+        </form>
     </div>
 </div>
     
