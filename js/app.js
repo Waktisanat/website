c52b4d6f28c27b448cd3329ce97f8c71eda173e4
Sébastien Leguevaques <sl@internethic.com>
2014-05-05 12:23:59 +0200

    Loader
    Ajout d'un Loader sur le chargement de la page et dès qu'on clic sur un
    lien.

--- js/app.js
+++ js/app.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
         evt.preventDefault();
         $('.f-dropdown').removeClass('open').css('left', '-99999px');
       });
+      
+    showLoader();
+      
+    $( "a" ).click(function() {
+        showLoader();
+    });
 });
+
+$(window).load( function(){
+    killLoader();
+});
+
+var showLoader = function(){
+     $('body').css('opacity', 0.5).append( "<img src='./images/ajax-loader.gif' id='ajax-loader' width='32px' height='32px' style='position: absolute; left: 49%; top: 49%;' />" );
+}
+
+var killLoader = function(){
+    $('body').css('opacity', 1);
+    $('#ajax-loader').remove();
+}
